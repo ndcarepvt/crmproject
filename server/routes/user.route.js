4f2406fdabd6f743bb07ec169f4ab4f172bf4b92
ndcarepvt <githubndcare@gmail.com>
2024-10-31 18:17:28 +0530

    ADD dotenv

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,7 +3,6 @@ import { loginUser, registerUser } from '../controller/user.controller.js';
 
 const userRouter = express.Router();
 
-
 // Define the route with multer middleware for file uploads
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
