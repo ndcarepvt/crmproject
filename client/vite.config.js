30f17f8783d4ed185efef2b3deb5920863ed0775
ndcarepvt <githubndcare@gmail.com>
2024-11-01 10:04:07 +0530

    add proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy :{
-      '/api':"https://crmproject-zeta.vercel.app"
+      '/api':{
+        target: 'https://crmproject-zeta.vercel.app',
+        changeOrigin: true,
+      }
     }
   },
   plugins: [react()],
