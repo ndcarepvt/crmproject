36a844b4435ca711ec12ef90274f457406368b6a
ndcarepvt <githubndcare@gmail.com>
2024-11-12 11:14:31 +0530

    Update role

--- client/src/pages/Invoice/Invoice.jsx
+++ client/src/pages/Invoice/Invoice.jsx
@@ -135,6 +135,7 @@ const Invoice = () => {
               <option value=''>-- Select Currency --</option>
               <option value='USD'>USD</option>
               <option value='GBP'>GBP</option>
+              <option value='EUR'>EUR</option>
 
             </select>
           </div>
