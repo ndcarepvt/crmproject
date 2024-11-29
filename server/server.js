cdfcb4d7d11cc96dd6b80c007ead8e2f9b4af80b
ndcarepvt <githubndcare@gmail.com>
2024-11-29 17:16:47 +0530

    Update Project

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express()
 // Intialize middleware
 app.use(express.json())
 app.use(cors({
-  origin: "*", // or '*' to allow all origins
+  origin: process.env.CLIENT_URL, // or '*' to allow all origins
   methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
   credentials: true, // If your frontend is sending cookies or authentication headers
   optionsSuccessStatus: 204 // For legacy browser support
