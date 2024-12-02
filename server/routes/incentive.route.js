0a2a6de840586a06d774b191338b07e9cd646cc2
ndcarepvt <githubndcare@gmail.com>
2024-12-02 12:53:32 +0530

    Add Authorized filter

--- server/routes/incentive.route.js
+++ server/routes/incentive.route.js
@@ -6,7 +6,7 @@ const incentiveRouter = express.Router();
 
 incentiveRouter.post('/add', authMiddleware,  addIncentive); // Route to add an incentive
 incentiveRouter.get('/getall', getAllIncentives); // Route to get all incentives
-incentiveRouter.get('/getfilter', authMiddleware, getIncentivesByNameAndRole); // Route to filter incentives by name and role
+incentiveRouter.post('/getfilter', authMiddleware, getIncentivesByNameAndRole); // Route to filter incentives by name and role
 incentiveRouter.post('/update', updateIncentive); 
 incentiveRouter.post('/datefilter', getIncentivesInDateRange);
 
